9ece1fdb06d64c87f5ac8719174263b447876239
ScrawnySquirrel <s.hyoung.238@gmail.com>
2016-03-08 08:25:56 -0800

    Implemented the basic GUI

--- ChatterBox/mainwindow.cpp
+++ ChatterBox/mainwindow.cpp
@@ -12,3 +12,42 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_msgInput_returnPressed()
+{
+    std::stringstream ss;
+    time_t rawtime;
+    struct tm *timeinfo;
+    const char *ampm;
+
+    // Get the local time
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    // Determine whether it's AM or PM
+    ampm = (timeinfo->tm_hour/12 > 1) ? "PM" : "AM";
+
+    // Build formatted output
+    ss << timeinfo->tm_hour%12 << ":" << std::setw(2) << std::setfill('0') << timeinfo->tm_min
+       << ":" << std::setw(2) << std::setfill('0') << timeinfo->tm_sec << ampm
+       << ": " << ui->msgInput->text().toStdString();
+
+    ui->msgDisplay->append(QString::fromStdString(ss.str()));
+
+    ui->msgInput->clear();
+}
+
+void MainWindow::on_sendBtn_clicked()
+{
+    MainWindow::on_msgInput_returnPressed();
+}
+
+void MainWindow::on_actionListWidget_triggered()
+{
+    ui->listUsers->addItem("User1\nVancouver, Canada");
+}
+
+void MainWindow::on_actionGraphicsView_triggered()
+{
+
+}
