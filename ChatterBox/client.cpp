5460fc3c9dd254351aa4ae27d26bf1983e2ea3a2
ScrawnySquirrel <s.hyoung.238@gmail.com>
2016-03-24 04:28:06 -0700

    Added comments headers

--- ChatterBox/client.cpp
+++ ChatterBox/client.cpp
@@ -1,3 +1,31 @@
+/*------------------------------------------------------------------------------------------------------------------
+-- SOURCE FILE: client.cpp
+--
+-- PROGRAM: ChatterBox
+--
+-- FUNCTIONS:
+-- explicit Client(QWidget *parent = 0);
+-- ~Client();
+-- void drawProfilePic();
+-- QString build_string(QString msg);
+-- void print_msg(QString msg);
+-- void scroll_to_bottom();
+-- void client_quit();
+-- void update_list();
+-- void on_msgInput_returnPressed();
+-- void on_sendBtn_clicked();
+-- void showEvent(QShowEvent * event);
+-- void resizeEvent(QResizeEvent * event);
+--
+-- DATE: March 10, 2016
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- NOTES:
+-- The client screen. Handles the GUI for all the client side. Including font styles, color, and profile pics.
+----------------------------------------------------------------------------------------------------------------------*/
 #include "client.h"
 #include "ui_client.h"
 #include "mainwindow.h"
@@ -5,6 +33,25 @@
 
 clientSrc *csrc;
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: Client
+--
+-- DATE: March 10, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 10, 2016 - Function created
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: Client::Client(QWidget *parent)
+--
+-- RETURNS: n/a
+--
+-- NOTES:
+-- The constructor for the Client class.
+----------------------------------------------------------------------------------------------------------------------*/
 Client::Client(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Client)
@@ -28,11 +75,49 @@ Client::Client(QWidget *parent) :
     csrc->clientStart();
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: ~Client
+--
+-- DATE: March 10, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 10, 2016 - Function created.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: Client::~Client()
+--
+-- RETURNS: n/a
+--
+-- NOTES:
+-- The Client class destructor.
+----------------------------------------------------------------------------------------------------------------------*/
 Client::~Client()
 {
     delete ui;
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: drawProfilePic
+--
+-- DATE: March 11, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 11, 2016 - Function created.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: void Client::drawProfilePic()
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Handles displaying the user's profile pic onto the screen. Uses the image path from the MainWindow.
+----------------------------------------------------------------------------------------------------------------------*/
 void Client::drawProfilePic() {
     imageObject = new QImage();
     imageObject->load(imagePath);
@@ -45,6 +130,25 @@ void Client::drawProfilePic() {
     showEvent(NULL);
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: showEvent
+--
+-- DATE: March 22, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 22, 2016 - Function created.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: void Client::showEvent(QShowEvent * event)
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Handles displaying the profile pic that fits inside the image screen. Keeps the image aspect.
+----------------------------------------------------------------------------------------------------------------------*/
 void Client::showEvent(QShowEvent * event)
 {
     this->QWidget::showEvent(event);
@@ -53,6 +157,26 @@ void Client::showEvent(QShowEvent * event)
     }
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: resizeEvent
+--
+-- DATE: March 22, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 22, 2016 - Function created.
+-- March 23, 2016 - Fix resizing to prevent crashes.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: void Client::resizeEvent(QResizeEvent * event)
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Handles resize event. Resizes the image based on the image screen. Keeps the image aspect.
+----------------------------------------------------------------------------------------------------------------------*/
 void Client::resizeEvent(QResizeEvent * event)
 {
     this->QWidget::resizeEvent(event);
@@ -61,6 +185,26 @@ void Client::resizeEvent(QResizeEvent * event)
     }
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: on_msgInput_returnPressed
+--
+-- DATE: March 11, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 11, 2016 - Function created.
+-- March 23, 2016 - Update to only handle append; move string building to separate function.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: void Client::on_msgInput_returnPressed()
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Handles when the user presses the return key. Gets the input field text and displays it onto the message screen.
+----------------------------------------------------------------------------------------------------------------------*/
 void Client::on_msgInput_returnPressed()
 {
     if(!ui->msgInput->text().isEmpty()) {
@@ -70,11 +214,50 @@ void Client::on_msgInput_returnPressed()
     }
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: on_sendBtn_clicked
+--
+-- DATE: March 11, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 11, 2016 - Function created.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: void Client::on_sendBtn_clicked()
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Handles when the user selects "Send" button. Has the same functionality as the returnPressed function.
+----------------------------------------------------------------------------------------------------------------------*/
 void Client::on_sendBtn_clicked()
 {
     Client::on_msgInput_returnPressed();
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: build_string
+--
+-- DATE: March 23, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 23, 2016 - Function created.
+-- March 23, 2016 - Updated to use structures.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: QString Client::build_string(QString msg)
+--
+-- RETURNS: QString - the formatted text string
+--
+-- NOTES:
+-- Takes in a string and returns a formatted string based on the font color and style that is currently selected.
+----------------------------------------------------------------------------------------------------------------------*/
 QString Client::build_string(QString msg) {
     std::stringstream ss;
     time_t rawtime;
@@ -101,12 +284,50 @@ QString Client::build_string(QString msg) {
     return QString::fromStdString(ss.str());
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: print_msg
+--
+-- DATE: March 23, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 23, 2016 - Function created
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: void Client::print_msg(QString msg)
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Displays the message onto the message screen and scrolls to the bottom to visibly show the last message.
+----------------------------------------------------------------------------------------------------------------------*/
 void Client::print_msg(QString msg)
 {
     ui->msgDisplay->append(msg);
     scroll_to_bottom();
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: scroll_to_bottom
+--
+-- DATE: March 23, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 23, 2016 - Function created.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: void Client::scroll_to_bottom()
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Moves the cursor to the end. Displays the last message appended to the screen.
+----------------------------------------------------------------------------------------------------------------------*/
 void Client::scroll_to_bottom()
 {
     QTextCursor c = ui->msgDisplay->textCursor();
@@ -114,10 +335,48 @@ void Client::scroll_to_bottom()
     ui->msgDisplay->setTextCursor(c);
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: client_quit
+--
+-- DATE: March 23, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 23, 2016 - Function created.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: void Client::client_quit()
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Write a message to the server to alert disconnect.
+----------------------------------------------------------------------------------------------------------------------*/
 void Client::client_quit() {
     csrc->writeMsg(MSG_QUIT, "Bye");
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: update_list
+--
+-- DATE: March 23, 2016
+--
+-- REVISIONS: (Date and Description)
+-- March 23, 2016 - Function created.
+--
+-- DESIGNER: Gabriel Lee
+--
+-- PROGRAMMER: Gabriel Lee
+--
+-- INTERFACE: void Client::update_list()
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Update the list of users connected to the server.
+----------------------------------------------------------------------------------------------------------------------*/
 void Client::update_list() {
     int j = 0;
     ui->userList->clear();
